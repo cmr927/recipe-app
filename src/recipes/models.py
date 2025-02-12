cb7be18c229e98f1ef6435439ccbb4a87f36d6b4
cmr927 <142535038+cmr927@users.noreply.github.com>
2025-02-11 22:16:42 -0500

    2.5 updated templates

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -23,3 +23,7 @@ class Recipe(models.Model):
 
     def get_absolute_url(self):
         return reverse ('recipes:detail', kwargs={'pk': self.pk})
+    
+    def get_ingredients(self):
+        print(self.ingredients.all())
+        return "\n".join([i.name for i in self.ingredients.all()])
