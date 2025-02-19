bcd56acc82f184b56b619f8b8b1940f4eb06a0b3
cmr927 <142535038+cmr927@users.noreply.github.com>
2025-02-18 21:51:27 -0500

    2.5 tests

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -2,19 +2,12 @@ from django.db import models
 from ingredients.models import Ingredient
 from django.shortcuts import reverse
 
-difficutly_choices = (
-    ('easy','Easy'),
-    ('medium', 'Meduim'),
-    ('intermediate', 'Intermediate'),
-    ('hard', 'Hard')
-)
-
 # Create your models here.
 class Recipe(models.Model):
     name= models.CharField(max_length=120)
     ingredients= models.ManyToManyField(Ingredient, related_name="recipes", through="recipe_ingredients.RecipeIngredient")
     cooking_time= models.PositiveIntegerField()
-    difficulty= models.CharField(max_length=20, choices=difficutly_choices)
+    difficulty= models.CharField(max_length=20)
     directions= models.TextField(default="directions")
     pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
     
@@ -25,4 +18,23 @@ class Recipe(models.Model):
         return reverse ('recipes:detail', kwargs={'pk': self.pk})
     
     def get_ingredients(self):
+        '''
+        Param: self
+        Displays the quantities and ingredients next to each other
+        '''  
         return "\n".join([i.quantity + ' ' + i.ingredient.name for i in self.recipeingredient_set.all()])
+    
+    def calc_difficulty (self):
+        '''
+        Param: self
+        Calculates the difficulty level of recipe. Retuns the difficulty of the recipe.
+        '''  
+        if self.cooking_time < 10 and len(self.recipeingredient_set.all()) < 4:
+            self.difficulty = "Easy"
+        elif self.cooking_time < 10 and len(self.recipeingredient_set.all()) >= 4:
+            self.difficulty = "Medium"
+        elif self.cooking_time >= 10 and len(self.recipeingredient_set.all()) < 4:
+            self.difficulty = "Intermediate"
+        elif self.cooking_time >= 10 and len(self.recipeingredient_set.all()) >= 4:
+            self.difficulty = "Hard"           
+        return self.difficulty
--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -1,16 +1,77 @@
 from django.test import TestCase
-from .models import Recipe, Ingredient #to access Recipe/Ingredient models
+from django.urls import reverse
+from .models import Recipe, Ingredient
+from recipe_ingredients.models import RecipeIngredient
 
 # Create your tests here.
 class RecipeModelTest(TestCase):
    
-   def setUpTestData():
+     def setUpTestData():
        #Set up non-modified objects used by all test methods
-       recipe = Recipe.objects.create(name= 'Peanut Butter & Jelly Sandwich', cooking_time= '5', difficulty= 'medium')
-       ingredients = Ingredient.objects.create(name= 'Peanut Butter, Jelly, Bread')
-       recipe.ingredients.add(ingredients)
+       recipe = Recipe.objects.create(name= 'Peanut Butter & Jelly Sandwich', cooking_time= 5, difficulty='Easy')
+       ingredient_1 = Ingredient.objects.create(name= 'Peanut Butter')
+       ingredient_2 = Ingredient.objects.create(name= 'Jelly')
+       ingredient_3 = Ingredient.objects.create(name='Bread')
+       ingredient_recipe_1 = RecipeIngredient.objects.create(ingredient=ingredient_1, recipe= recipe, quantity="2oz")
+       ingredient_recipe_2 = RecipeIngredient.objects.create(ingredient=ingredient_2, recipe= recipe, quantity="1oz")
+       ingredient_recipe_3 = RecipeIngredient.objects.create(ingredient=ingredient_3, recipe= recipe, quantity="2 slices")
        
-   def test_recipe_name(self):
+       recipe.ingredients.add(ingredient_1)
+       recipe.ingredients.add(ingredient_2)
+       recipe.ingredients.add(ingredient_3)
+   
+   
+     def test_recipe_ingredients(self):
+        #Testing the recipe has the correct number of ingredients
+        recipe = Recipe.objects.get(id=1)
+        
+        self.assertEqual(len(recipe.ingredients.all()), 3)
+        
+     def test_get_ingredients(self):
+          #Testing if the the quantities and ingredients are next to each other
+          recipe = Recipe.objects.get(id=1)
+          ingredients= recipe.get_ingredients()
+          self.assertEqual(ingredients, '2oz Peanut Butter\n1oz Jelly\n2 slices Bread')
+             
+        
+     def test_calc_difficulty_easy(self): 
+          # Testing if the Easy difficulty level is calculated
+          recipe = Recipe.objects.get(id=1)
+          recipe.difficulty=""
+          self.assertEqual(recipe.difficulty, "")
+          recipe.calc_difficulty()
+          self.assertEqual(recipe.difficulty, 'Easy')
+          
+     def test_calc_difficulty_medium(self): 
+          # Testing if the Medium difficulty level is calculated
+          recipe = Recipe.objects.get(id=1)
+          recipe.difficulty=""
+          recipe.cooking_time=5
+          ingredient_4 = Ingredient.objects.create(name='Banana')
+          recipe.ingredients.add(ingredient_4)
+          recipe.calc_difficulty()
+          self.assertEqual(recipe.difficulty, 'Medium')
+          
+     def test_calc_difficulty_intermediate(self): 
+          # Testing if the Intermediate difficulty level is calculated
+          recipe = Recipe.objects.get(id=1)
+          recipe.difficulty=""
+          recipe.cooking_time=10
+          recipe.calc_difficulty()
+          self.assertEqual(recipe.difficulty, 'Intermediate')     
+          
+     def test_calc_difficulty_hard(self): 
+          # Testing if the Hard difficulty level is calculated
+          recipe = Recipe.objects.get(id=1)
+          recipe.difficulty=""
+          recipe.cooking_time=10
+          ingredient_4 = Ingredient.objects.create(name='Banana')
+          recipe.ingredients.add(ingredient_4)
+          recipe.calc_difficulty()
+          self.assertEqual(recipe.difficulty, 'Hard')     
+          
+            
+     def test_recipe_name(self):
         #Get a recipe object to test
         recipe = Recipe.objects.get(id=1)
         
@@ -20,7 +81,7 @@ class RecipeModelTest(TestCase):
         #Compare the value to the expected result
         self.assertEqual(field_lable, 'name') 
  
-   def test_name_max_length(self):
+     def test_name_max_length(self):
         #Get a recipe object to test
         recipe = Recipe.objects.get(id=1)
         
@@ -30,7 +91,7 @@ class RecipeModelTest(TestCase):
         #Compare the value to the expected result i.e. 120
         self.assertEqual(max_length, 120) 
         
-   def test_difficulty_max_length(self):
+     def test_difficulty_max_length(self):
         #Get a recipe object to test
         recipe = Recipe.objects.get(id=1)
         
@@ -39,3 +100,20 @@ class RecipeModelTest(TestCase):
         
         #Compare the value to the expected result i.e. 20
         self.assertEqual(max_length, 20) 
+   
+     def test_get_absolute_url_1(self):
+       recipe = Recipe.objects.get(id=1)
+       #get_absolute_url() should take you to the detail page of recipe #1
+       #and load the URL /recipes/list/1
+       self.assertEqual(recipe.get_absolute_url(), '/recipes/list/1')   
+  
+#    def test_get_absolute_url_2(self):
+#         recipe = Recipe.objects.get(id=2)
+#         #get_absolute_url() should take you to the detail page of recipe #2
+#         # and load the URL /recipes/list/2
+#         self.assertEqual(recipe.get_absolute_url(), '/recipes/list/2')   
+       
+#    def test_get_absolute_url_pk(self):
+#         # Ensures the method correctly returns the absolute URL for a recipe detail view.
+#         expected_url = reverse('recipes:detail', kwargs={'pk': self.recipe.pk})
+#         self.assertEqual(self.recipe.get_absolute_url(), expected_url)
