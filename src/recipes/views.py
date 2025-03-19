8e2f0eb1229eccaf97447e1c2929c5924cfbffd7
cmr927 <142535038+cmr927@users.noreply.github.com>
2025-03-18 21:59:37 -0400

    2.7 search functionality in progress

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render     #imported by default
 from django.views.generic import ListView, DetailView   #to display lists and details
+from .forms import RecipesSearchForm
 from .models import Recipe    #to access Recipe model
-#to protect class-based view
-from django.contrib.auth.mixins import LoginRequiredMixin
+from .utils import get_recipename_from_id, get_chart
+from django.contrib.auth.mixins import LoginRequiredMixin #to protect class-based view
+import pandas as pd
 
 # Create your views here.
 class RecipeListView(LoginRequiredMixin, ListView):             #class-based “protected” view
@@ -12,6 +14,68 @@ class RecipeListView(LoginRequiredMixin, ListView):             #class-based “
 class RecipeDetailView(LoginRequiredMixin, DetailView):         #class-based “protected” view
    model = Recipe                           #specify model
    template_name = 'recipes/detail.html'    #specify template
+   
+
+#define function-based view - records()
+def records(request):
+   #create an instance of RecipesSearchForm that you defined in recipes/forms.py
+   form = RecipesSearchForm(request.POST or None)
+   recipes_df=None     #initialize dataframe to None
+   chart=None        #initialize chart to None
+   
+   #check if the button is clicked
+   if request.method =='POST':
+       #read recipe_title and chart_type
+       recipe_title = request.POST.get('recipe_title')
+       chart_type = request.POST.get('chart_type')
+       
+       #apply filter to extract data
+       qs =Recipe.objects.filter(recipe__name=recipe_title)
+       if qs:      #if data found
+           #convert the queryset values to pandas dataframe
+           recipes_df=pd.DataFrame(qs.values())
+           #convert the ID to Name of recipe
+           recipes_df['recipe_id']=recipes_df['recipe_id'].apply(get_recipename_from_id)
+           
+           #call get_chart by passing chart_type from user input, recipess dataframe and labels
+           chart=get_chart(chart_type, recipes_df, labels=recipes_df['date_created'].values)
+           
+           #convert the dataframe to HTML
+           recipes_df=recipes_df.to_html()
+       
+       
+       #display in terminal - needed for debugging during development only
+       print (recipe_title, chart_type)
+       
+       print ('Exploring querysets:')
+       print ('Case 1: Output of Recipe.objects.all()')
+       qs=Recipe.objects.all()
+       print (qs)
+
+       print ('Case 2: Output of Recipe.objects.filter(recipe_name=recipe_title)')
+       qs =Recipe.objects.filter(recipe__name=recipe_title)
+       print (qs)
+
+       print ('Case 3: Output of qs.values')
+       print (qs.values())
+
+       print ('Case 4: Output of qs.values_list()')
+       print (qs.values_list())
+
+       print ('Case 5: Output of Recipe.objects.get(id=1)')
+       obj = Recipe.objects.get(id=1)
+       print (obj)
+
+   #pack up data to be sent to template in the context dictionary
+   context={
+           'form': form,
+           'recipes_df': recipes_df,
+           'chart': chart,
+   }
+   
+
+   #CHANGE THIS! load the sales/record.html page using the data that you just prepared
+   return render(request, 'sales/records.html', context)   
        
 #Maybe get rid of this? Might be old.
 def home(request):
