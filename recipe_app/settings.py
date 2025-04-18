bfd62489601cf3864e77f17387184ed33abdf452
cmr927 <142535038+cmr927@users.noreply.github.com>
2025-04-18 13:16:06 -0400

    add the procfile

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["shielded-badlands-21677-49d691812b2d.herokuapp.com"]
+ALLOWED_HOSTS = ["shielded-badlands-21677-49d691812b2d.herokuapp.com", "localhost"]
 
 
 # Application definition
