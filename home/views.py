a0efef0dd31afcea6c3f3fa2b3bcd9d65e134045
cmr927 <142535038+cmr927@users.noreply.github.com>
2025-04-23 21:07:07 -0400

    2.8 django_on_heroku

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import logging
 
 # Create your views here.
 
 
 def home(request):
+    logging.error("TEST TEST TEST")
+    logging.error("TEST TEST TEST")
+    logging.error("TEST TEST TEST")
+    logging.error("TEST TEST TEST")
+    logging.error("TEST TEST TEST")
+    logging.error("TEST TEST TEST")
     return render(request, "home/home.html")
